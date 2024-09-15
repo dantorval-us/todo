d961b96407e1732d3828b92d0406a610e15f5cdc
dantorval-us <dantorval@alum.us.es>
2024-09-15 17:50:23 +0200

    style/mod navbar
    Incluye usuario y marca la funcionalidad activa

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { User } from '@angular/fire/auth';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { AuthService } from '@services/auth.service';
-import { TareaService } from '@services/tarea.service';
+import { PrimeraPalabraPipe } from '@shared/primera-palabra.pipe';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, PrimeraPalabraPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
   usuario: User | null;
+  currentRoute: string = '';
 
-  private readonly _tareaService = inject(TareaService);
-
-  constructor (private readonly _authService: AuthService) {
+  constructor (
+    private _router: Router,
+    private readonly _authService: AuthService
+  ) {
     this.usuario = this._authService.getCurrentUser();
+  }
+
+  ngOnInit() {
+    this.getCurrentRoute();    
+  }
+
+  getCurrentRoute(): void {
+    this._router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.currentRoute = event.urlAfterRedirects;
+    });
   }
 
   handleLogout(): void {
